bdf034a75de6252f2e7ac195466024817a89ca87
RaymondMeng <2391530014@qq.com>
2024-04-27 22:31:34 +0800

    ✨ feat: add some temp
    finish some design,next is verification

--- rtl/Multiport_sBuffer_top.v
+++ rtl/Multiport_sBuffer_top.v
@@ -59,16 +59,124 @@ module Multiport_sBuffer_top(
     input       [`DATA_WIDTH-1:0]   p4_wr_data,
     
     output                          p4_almost_full,
-    output                          p4_full
+    output                          p4_full,
+
+    /*port5 interface*/
+    input                           p5_wr_sop,
+    input                           p5_wr_eop,
+    input                           p5_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p5_wr_data,
+    
+    output                          p5_almost_full,
+    output                          p5_full,
+
+    /*port6 interface*/
+    input                           p6_wr_sop,
+    input                           p6_wr_eop,
+    input                           p6_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p6_wr_data,
+    
+    output                          p6_almost_full,
+    output                          p6_full,
+
+    /*port7 interface*/
+    input                           p7_wr_sop,
+    input                           p7_wr_eop,
+    input                           p7_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p7_wr_data,
+    
+    output                          p7_almost_full,
+    output                          p7_full,
+
+    /*port8 interface*/
+    input                           p8_wr_sop,
+    input                           p8_wr_eop,
+    input                           p8_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p8_wr_data,
+    
+    output                          p8_almost_full,
+    output                          p8_full,
+
+    /*port9 interface*/
+    input                           p9_wr_sop,
+    input                           p9_wr_eop,
+    input                           p9_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p9_wr_data,
+    
+    output                          p9_almost_full,
+    output                          p9_full,
+
+    /*port10 interface*/
+    input                           p10_wr_sop,
+    input                           p10_wr_eop,
+    input                           p10_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p10_wr_data,
+    
+    output                          p10_almost_full,
+    output                          p10_full,
+
+    /*port11 interface*/
+    input                           p11_wr_sop,
+    input                           p11_wr_eop,
+    input                           p11_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p11_wr_data,
+    
+    output                          p11_almost_full,
+    output                          p11_full,
+
+    /*port12 interface*/
+    input                           p12_wr_sop,
+    input                           p12_wr_eop,
+    input                           p12_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p12_wr_data,
+    
+    output                          p12_almost_full,
+    output                          p12_full,
+
+    /*port13 interface*/
+    input                           p13_wr_sop,
+    input                           p13_wr_eop,
+    input                           p13_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p13_wr_data,
+    
+    output                          p13_almost_full,
+    output                          p13_full,
+
+    /*port14 interface*/
+    input                           p14_wr_sop,
+    input                           p14_wr_eop,
+    input                           p14_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p14_wr_data,
+    
+    output                          p14_almost_full,
+    output                          p14_full,
+
+    /*port15 interface*/
+    input                           p15_wr_sop,
+    input                           p15_wr_eop,
+    input                           p15_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p15_wr_data,
+    
+    output                          p15_almost_full,
+    output                          p15_full,
+
+    /*port16 interface*/
+    input                           p16_wr_sop,
+    input                           p16_wr_eop,
+    input                           p16_wr_vld,
+    input       [`DATA_WIDTH-1:0]   p16_wr_data,
+    
+    output                          p16_almost_full,
+    output                          p16_full
 
     );
 
-wire [`DATA_WIDTH-1:0] p1_dout, p2_dout, p3_dout, p4_dout;
-wire p1_empty, p2_empty, p3_empty, p4_empty;
-wire p1_wr_en, p2_wr_en, p3_wr_en, p4_wr_en;
-wire p1_rd_en, p2_rd_en, p3_rd_en, p4_rd_en;
-wire [`DATA_WIDTH-1:0] sdata;
-wire sdat_valid;
+wire [`DATA_WIDTH-1:0] p1_dout, p2_dout, p3_dout, p4_dout, p5_dout, p6_dout, p7_dout, p8_dout, p9_dout, p10_dout, p11_dout, p12_dout, p13_dout, p14_dout, p15_dout, p16_dout;
+wire p1_empty, p2_empty, p3_empty, p4_empty, p5_empty, p6_empty, p7_empty, p8_empty, p9_empty, p10_empty, p11_empty, p12_empty, p13_empty, p14_empty, p15_empty, p16_empty;
+wire p1_wr_en, p2_wr_en, p3_wr_en, p4_wr_en, p5_wr_en, p6_wr_en, p7_wr_en, p8_wr_en, p9_wr_en, p10_wr_en, p11_wr_en, p12_wr_en, p13_wr_en, p14_wr_en, p15_wr_en, p16_wr_en;
+wire p1_rd_en, p2_rd_en, p3_rd_en, p4_rd_en, p5_rd_en, p6_rd_en, p7_rd_en, p8_rd_en, p9_rd_en, p10_rd_en, p11_rd_en, p12_rd_en, p13_rd_en, p14_rd_en, p15_rd_en, p16_rd_en;
+// wire [`DATA_WIDTH-1:0] sdata;
+// wire sdat_valid;
 //wire [7:0] p1_dat_count, p2_dat_count, p3_dat_count, p4_dat_count;
 
 /*fifo read latency: one cycle*/
@@ -120,28 +228,174 @@ port_input_fifo port4_input_inst (
   .empty(p4_empty)              // output wire empty
 );
 
-enter_arbitr enter_arbitr_inst(
-    .clk                      (clk),      
-    .rst_n                    (rst_n), 
-    /*fifo1 interface*/ 
-    .i_fifo1_empty            (p1_empty),
-    .i_fifo1_data             (p1_dout),
-    .o_fifo1_rd_en            (p1_rd_en),
-    /*fifo2 interface*/
-    .i_fifo2_empty            (p2_empty),
-    .i_fifo2_data             (p2_dout),
-    .o_fifo2_rd_en            (p2_rd_en),
-    /*fifo3 interface*/
-    .i_fifo3_empty            (p3_empty),
-    .i_fifo3_data             (p3_dout),
-    .o_fifo3_rd_en            (p3_rd_en),
-    /*fifo4 interface*/
-    .i_fifo4_empty            (p4_empty),
-    .i_fifo4_data             (p4_dout),
-    .o_fifo4_rd_en            (p4_rd_en),
-    .o_sdata                  (sdata),
-    .o_data_valid             (sdat_valid)
-    );
+port_input_fifo port5_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p5_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p5_wr_vld),              // input wire wr_en
+  .rd_en(p5_rd_en),              // input wire rd_en
+  .dout(p5_dout),                // output wire [63 : 0] dout
+  .full(p5_full),                // output wire full
+  .almost_full(p5_almost_full),  // output wire almost_full
+  .empty(p5_empty)              // output wire empty
+);
+
+port_input_fifo port6_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p6_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p6_wr_vld),              // input wire wr_en
+  .rd_en(p6_rd_en),              // input wire rd_en
+  .dout(p6_dout),                // output wire [63 : 0] dout
+  .full(p6_full),                // output wire full
+  .almost_full(p6_almost_full),  // output wire almost_full
+  .empty(p6_empty)              // output wire empty
+);
+
+port_input_fifo port7_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p7_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p7_wr_vld),              // input wire wr_en
+  .rd_en(p7_rd_en),              // input wire rd_en
+  .dout(p7_dout),                // output wire [63 : 0] dout
+  .full(p7_full),                // output wire full
+  .almost_full(p7_almost_full),  // output wire almost_full
+  .empty(p7_empty)              // output wire empty
+);
+
+port_input_fifo port8_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p8_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p8_wr_vld),              // input wire wr_en
+  .rd_en(p8_rd_en),              // input wire rd_en
+  .dout(p8_dout),                // output wire [63 : 0] dout
+  .full(p8_full),                // output wire full
+  .almost_full(p8_almost_full),  // output wire almost_full
+  .empty(p8_empty)              // output wire empty
+);
+
+port_input_fifo port9_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p9_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p9_wr_vld),              // input wire wr_en
+  .rd_en(p9_rd_en),              // input wire rd_en
+  .dout(p9_dout),                // output wire [63 : 0] dout
+  .full(p9_full),                // output wire full
+  .almost_full(p9_almost_full),  // output wire almost_full
+  .empty(p9_empty)              // output wire empty
+);
+
+port_input_fifo port10_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p10_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p10_wr_vld),              // input wire wr_en
+  .rd_en(p10_rd_en),              // input wire rd_en
+  .dout(p10_dout),                // output wire [63 : 0] dout
+  .full(p10_full),                // output wire full
+  .almost_full(p10_almost_full),  // output wire almost_full
+  .empty(p10_empty)              // output wire empty
+);
+
+port_input_fifo port11_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p11_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p11_wr_vld),              // input wire wr_en
+  .rd_en(p11_rd_en),              // input wire rd_en
+  .dout(p11_dout),                // output wire [63 : 0] dout
+  .full(p11_full),                // output wire full
+  .almost_full(p11_almost_full),  // output wire almost_full
+  .empty(p11_empty)              // output wire empty
+);
+
+port_input_fifo port12_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p12_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p12_wr_vld),              // input wire wr_en
+  .rd_en(p12_rd_en),              // input wire rd_en
+  .dout(p12_dout),                // output wire [63 : 0] dout
+  .full(p12_full),                // output wire full
+  .almost_full(p12_almost_full),  // output wire almost_full
+  .empty(p12_empty)              // output wire empty
+);
+
+port_input_fifo port13_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p13_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p13_wr_vld),              // input wire wr_en
+  .rd_en(p13_rd_en),              // input wire rd_en
+  .dout(p13_dout),                // output wire [63 : 0] dout
+  .full(p13_full),                // output wire full
+  .almost_full(p13_almost_full),  // output wire almost_full
+  .empty(p13_empty)              // output wire empty
+);
+
+port_input_fifo port14_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p14_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p14_wr_vld),              // input wire wr_en
+  .rd_en(p14_rd_en),              // input wire rd_en
+  .dout(p14_dout),                // output wire [63 : 0] dout
+  .full(p14_full),                // output wire full
+  .almost_full(p14_almost_full),  // output wire almost_full
+  .empty(p14_empty)              // output wire empty
+);
+
+port_input_fifo port15_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p15_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p15_wr_vld),              // input wire wr_en
+  .rd_en(p15_rd_en),              // input wire rd_en
+  .dout(p15_dout),                // output wire [63 : 0] dout
+  .full(p15_full),                // output wire full
+  .almost_full(p15_almost_full),  // output wire almost_full
+  .empty(p15_empty)              // output wire empty
+);
+
+port_input_fifo port16_input_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(p16_wr_data),                  // input wire [63 : 0] din
+  .wr_en(p16_wr_vld),              // input wire wr_en
+  .rd_en(p16_rd_en),              // input wire rd_en
+  .dout(p16_dout),                // output wire [63 : 0] dout
+  .full(p16_full),                // output wire full
+  .almost_full(p16_almost_full),  // output wire almost_full
+  .empty(p16_empty)              // output wire empty
+);
+
+
+
+// enter_arbitr enter_arbitr_inst(
+//     .clk                      (clk),      
+//     .rst_n                    (rst_n), 
+//     /*fifo1 interface*/ 
+//     .i_fifo1_empty            (p1_empty),
+//     .i_fifo1_data             (p1_dout),
+//     .o_fifo1_rd_en            (p1_rd_en),
+//     /*fifo2 interface*/
+//     .i_fifo2_empty            (p2_empty),
+//     .i_fifo2_data             (p2_dout),
+//     .o_fifo2_rd_en            (p2_rd_en),
+//     /*fifo3 interface*/
+//     .i_fifo3_empty            (p3_empty),
+//     .i_fifo3_data             (p3_dout),
+//     .o_fifo3_rd_en            (p3_rd_en),
+//     /*fifo4 interface*/
+//     .i_fifo4_empty            (p4_empty),
+//     .i_fifo4_data             (p4_dout),
+//     .o_fifo4_rd_en            (p4_rd_en)
+//     // .o_sdata                  (sdata),
+//     // .o_data_valid             (sdat_valid)
+//     );
 
 
 
--- rtl/enter_arbitr.v
+++ rtl/enter_arbitr.v
@@ -15,7 +15,7 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments:  need verification
 // 
 //////////////////////////////////////////////////////////////////////////////////
 `include "defines.v"
@@ -23,79 +23,240 @@
 module enter_arbitr(
     input                                           clk,      
     input                                           rst_n,  
+
     /*fifo1 interface*/
     input                                           i_fifo1_empty,
     input               [`DATA_WIDTH-1:0]           i_fifo1_data,
     output                                          o_fifo1_rd_en,
+
     /*fifo2 interface*/
     input                                           i_fifo2_empty,
     input               [`DATA_WIDTH-1:0]           i_fifo2_data,
     output                                          o_fifo2_rd_en,
+
     /*fifo3 interface*/
     input                                           i_fifo3_empty,
     input               [`DATA_WIDTH-1:0]           i_fifo3_data,
     output                                          o_fifo3_rd_en,
+
     /*fifo4 interface*/
     input                                           i_fifo4_empty,
     input               [`DATA_WIDTH-1:0]           i_fifo4_data,
     output                                          o_fifo4_rd_en,
-    /*serial data output control*/
-    output              [`DATA_WIDTH-1:0]           o_sdata,
-    output                                          o_data_valid
+
+    /*DataBuffer fifo interface*/
+    input                                           i_DBfifo_rd_en,
+    output                                          o_DBfifo_empty,
+    output              [`DATA_WIDTH-1:0]           o_DBfifo_data
+
+    // /*serial data output control*/
+    // output              [`DATA_WIDTH-1:0]           o_sdata,
+    // output                                          o_data_valid
     );
 
-reg [1:0] cnt;
-reg fifo1_rd_en, fifo2_rd_en, fifo3_rd_en, fifo4_rd_en;
-reg fifo1_rd_en_d, fifo2_rd_en_d, fifo3_rd_en_d, fifo4_rd_en_d;
+reg [10:0] dat_num; //单位是字节
 
-assign o_fifo1_rd_en = fifo1_rd_en;
-assign o_fifo2_rd_en = fifo2_rd_en;
-assign o_fifo3_rd_en = fifo3_rd_en;
-assign o_fifo4_rd_en = fifo4_rd_en;
 
-/* 2bit-count & fifo_rd_en信号打拍 */
+`ifdef PACKET_SWITCH
+
+reg [2:0] state;
+reg [1:0] RR; //公平轮询寄存器
+parameter IDLE = 3'b100, s0 = 3'b000, s1 = 3'b001, s2 = 3'b011, s3 = 3'b010, s4 = 3'b110, s5 = 3'b111;
+
+wire [`DATA_WIDTH-1:0] i_fifo_data;
+reg fifo_rd_en;
+reg [7:0] unit_cnt; //最多128个64位信元
+reg [1:0] sel; //片选 ，选择哪个fifo读
+reg [`DATA_WIDTH-1:0] sdata;
+reg sdata_wr_en, ptr_wr_en;
+
+// reg [`DATA_WIDTH/4-1:0] ptr_dat; //16位指针
+// reg [`DATA_WIDTH/4-1:0] ptr_dat_temp; //16位指针临时变量
+
+reg [`DATA_WIDTH-1:0]   DBfifo_din;
+
+/* | 17 ~ 7 |  6 ~ 4   |   3 ~ 0   | */
+/* | length | priority | dest_port | */
+reg [`DATA_WIDTH-1:0]   pkg_head; 
+
+/* | 18 ~ 15 | 14 ~ 7 |  6 ~ 4   |  3 ~ 0    | */
+/* | in_port | length | priority | dest_port | */
+/*           64bit nums                        */
+wire [`DATA_WIDTH-1:0]   pkg_head_update; //更新之后的包头数据结构
+
+wire DBfifo_full, DBfifo_almost_full;
+reg DBfifo_wr_en;
+
+//直接一段式
 always @(posedge clk or negedge rst_n) begin
     if (rst_n == 1'b0) begin
-        cnt <= 'd0;
-        fifo1_rd_en_d <= 1'b0;
-        fifo2_rd_en_d <= 1'b0;
-        fifo3_rd_en_d <= 1'b0;
-        fifo4_rd_en_d <= 1'b0;
+        state <= s0;
+        RR <= 'd0;
+        sdata_wr_en <= 1'b0;
     end
     else begin
-        cnt <= cnt + 1'b1;
-        fifo1_rd_en_d <= fifo1_rd_en;
-        fifo2_rd_en_d <= fifo2_rd_en;
-        fifo3_rd_en_d <= fifo3_rd_en;
-        fifo4_rd_en_d <= fifo4_rd_en;
+        case (state)
+            s0: begin //决定轮询顺序
+                case (RR)
+                    2'd0: begin
+                        if (~i_fifo1_empty) begin //从fifo1开始轮询
+                            sel <= 'd0; //选中第一个FIFO
+                            fifo_rd_en <= 1'b1; //读使能拉高
+                            state <= s1; //跳转s1
+                        end
+                        else if (~i_fifo2_empty) begin
+                            sel <= 'd1;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo3_empty) begin
+                            sel <= 'd2;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo4_empty) begin
+                            sel <= 'd3;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                    end
+                    2'd1: begin
+                        if (~i_fifo2_empty) begin
+                            sel <= 'd1;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo3_empty) begin
+                            sel <= 'd2;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo4_empty) begin
+                            sel <= 'd3;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo1_empty) begin //从fifo1开始轮询
+                            sel <= 'd0; //选中第一个FIFO
+                            fifo_rd_en <= 1'b1; //读使能拉高
+                            state <= s1; //跳转s1
+                        end
+                    end
+                    2'd2: begin
+                        if (~i_fifo3_empty) begin
+                            sel <= 'd2;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo4_empty) begin
+                            sel <= 'd3;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo1_empty) begin //从fifo1开始轮询
+                            sel <= 'd0; //选中第一个FIFO
+                            fifo_rd_en <= 1'b1; //读使能拉高
+                            state <= s1; //跳转s1
+                        end
+                        else if (~i_fifo2_empty) begin
+                            sel <= 'd1;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                    end
+                    2'd3: begin
+                        if (~i_fifo4_empty) begin
+                            sel <= 'd3;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo1_empty) begin //从fifo1开始轮询
+                            sel <= 'd0; //选中第一个FIFO
+                            fifo_rd_en <= 1'b1; //读使能拉高
+                            state <= s1; //跳转s1
+                        end
+                        else if (~i_fifo2_empty) begin
+                            sel <= 'd1;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                        else if (~i_fifo3_empty) begin
+                            sel <= 'd2;
+                            fifo_rd_en <= 1'b1;
+                            state <= s1;
+                        end
+                    end
+                endcase
+            end
+            s1: begin //轮询参数变化
+                RR <= RR + 1;
+                state <= s2;
+            end
+            /* 开始读包数据 */
+            s2: begin //读包头更改数据写入databuffer  这里可能时序不太好，仿真看看如果不行继续拆分状态
+                pkg_head <= i_fifo_data;
+                unit_cnt <= i_fifo_data[17:7] >> 3;//包中64bit信元的个数
+                //pkg_head_update <= {, i_fifo_data[17:7] >> 3, i_fifo_data[6:0]};
+                if (~DBfifo_full) begin //写入包头进databuffer
+                    DBfifo_wr_en <= 1'b1; //写入data buffer fifo使能
+                    DBfifo_din <= pkg_head_update; //data buffer fifo寄存数据
+                end
+                state <= s3;
+            end
+            s3: begin //循环读包数据并写入databuffer
+                if (unit_cnt == 'd2) begin //读完包的倒数第二个信元，跳入下个状态
+                    state <= s4;
+                end
+                unit_cnt <= unit_cnt - 1'b1;
+                if (~DBfifo_full) begin
+                    DBfifo_wr_en <= 1'b1; //写入data buffer fifo使能
+                    DBfifo_din <= i_fifo_data; //data buffer fifo寄存数据
+                end
+            end
+            s4: begin //读最后一个数据，关闭fifo读取
+                unit_cnt <= unit_cnt - 1'b1; //dat_num结果为0
+                if (~DBfifo_full) begin
+                    DBfifo_wr_en <= 1'b1;
+                    DBfifo_din <= i_fifo_data;
+                end
+                fifo_rd_en <= 1'b0; //关闭fifo读取使能
+                state <= s5;
+            end
+            s5: begin //关闭db fifo写使能
+                DBfifo_wr_en <= 1'b0; //关闭data buffer fifo写使能
+                state <= s0;
+            end
+        endcase
     end
 end
 
-/*mux*/
-always @(*) begin
-    fifo1_rd_en = 'b0;
-    fifo2_rd_en = 'b0;
-    fifo3_rd_en = 'b0;
-    fifo4_rd_en = 'b0;
-    if (cnt == 2'd0) begin
-        fifo1_rd_en = ~i_fifo1_empty;
-    end
-    else if(cnt == 2'd1) begin
-        fifo2_rd_en = ~i_fifo2_empty;
-    end
-    else if(cnt == 2'd2) begin
-        fifo3_rd_en = ~i_fifo3_empty;
-    end
-    else if(cnt == 2'd3) begin
-        fifo4_rd_en = ~i_fifo4_empty;
-    end
-end
+//这里可能时序不太好，仿真看看如果不行继续拆分状态
+assign pkg_head_update = {sel, unit_cnt, i_fifo_data[6:0]};
 
-assign o_data_valid = fifo1_rd_en_d | fifo2_rd_en_d | fifo3_rd_en_d | fifo4_rd_en_d;
-assign o_sdata = fifo1_rd_en_d ? i_fifo1_data :
-                (fifo2_rd_en_d ? i_fifo2_data :
-                (fifo3_rd_en_d ? i_fifo3_data :
-                (fifo4_rd_en_d ? i_fifo4_data : 64'dz)));
+port_input_fifo data_buffer_inst (
+  .clk(clk),                  // input wire clk
+  .srst(~rst_n),                // input wire srst
+  .din(DBfifo_din),                  // input wire [63 : 0] din
+  .wr_en(DBfifo_wr_en),              // input wire wr_en
+  .rd_en(i_DBfifo_rd_en),              // input wire rd_en
+  .dout(o_DBfifo_data),                // output wire [63 : 0] dout
+  .full(DBfifo_full),                // output wire full
+  .almost_full(DBfifo_almost_full),  // output wire almost_full
+  .empty(o_DBfifo_empty)              // output wire empty
+);
 
+assign o_fifo1_rd_en = fifo_rd_en & (sel == 'd0);
+assign o_fifo2_rd_en = fifo_rd_en & (sel == 'd1);
+assign o_fifo3_rd_en = fifo_rd_en & (sel == 'd2);
+assign o_fifo4_rd_en = fifo_rd_en & (sel == 'd3);
+
+assign i_fifo_data = (sel == 'd0) ? i_fifo1_data :
+                     (sel == 'd1) ? i_fifo2_data :
+                     (sel == 'd2) ? i_fifo3_data : i_fifo4_data;
+
+
+
+
+`endif
 
 endmodule
